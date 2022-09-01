c35b1aa44e996e4137e063ee8242f135f61d0252
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2022-09-01 02:17:23 -0400

    Neovim: Add highlight on yank

--- neovim/.config/nvim/lua/user/options.lua
+++ neovim/.config/nvim/lua/user/options.lua
@@ -7,19 +7,6 @@ opt.cmdheight = 2      -- Use two screen lines for the command line (helps avoid
 opt.confirm = true     -- Present a dialog instead of failing a command due to unsaved changes
 opt.linebreak = true   -- When `wrap` is on, visually break lines only at specific characters
 opt.mouse = 'a'        -- Enable mouse support in all modes
-
-opt.number = true          -- Display line numbers
-opt.relativenumber = true  -- Display relative line numbers
-
--- Switch to absolute line numbers when a window loses focus
-cmd([[
-    augroup number-toggle
-        autocmd!
-        autocmd WinEnter * set relativenumber
-        autocmd WinLeave * set norelativenumber
-    augroup END
-]])
-
 opt.scrolloff = 4      -- Keep some lines visible above/below the cursor
 opt.sidescrolloff = 4  -- Keep some columns visible to the left/right of the cursor
 opt.showmatch = true   -- Show matching brace/bracket/parenthesis
@@ -28,6 +15,27 @@ opt.splitright = true  -- Open new windows to the right of the current one
 opt.termguicolors = true   -- 24-bit color support in the TUI
 
 cmd('colorscheme nord')    -- Set colorscheme
+
+-- Hightlight selection on yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+    -- group = vim.api.nvim_create_augroup('highlight_yank'),
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank { timeout=250 }
+    end,
+})
+
+-- Display relative line numbers,
+-- but switch to absolute line numbers when a window loses focus
+opt.number = true
+opt.relativenumber = true
+cmd([[
+    augroup number-toggle
+        autocmd!
+        autocmd WinEnter * set relativenumber
+        autocmd WinLeave * set norelativenumber
+    augroup END
+]])
 
 
 -- [[ Searching ]]
