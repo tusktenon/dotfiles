ff9fc75b528152f95cacf6b5a7c934124b7ceb47
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2022-09-13 16:34:35 -0400

    Neovim: Add settings for Git commit messages

---
+++ neovim/.config/nvim/after/ftplugin/gitcommit.lua
@@ -0,0 +1,13 @@
+--[[
+Per Tim Pope's 'A Note About Git Commit Messages'
+(https://tbaggery.com/2008/04/19/a-note-about-git-commit-messages.html):
+  * Summary line of 50 characters or less, followed by a blank line.
+  * Detailed explanatory text, if necessary, wrapped at 72 characters.
+  * Use a hanging indent for bullet points.
+--]]
+
+local opt = vim.opt
+
+opt.colorcolumn = '50,72'
+opt.textwidth = 72
+opt.expandtab = true
