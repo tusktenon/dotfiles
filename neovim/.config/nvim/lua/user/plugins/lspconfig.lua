428d2590962ae748b1d4e7496a4b2b175720f97f
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2022-09-16 09:29:46 -0400

    Neovim: Refactor buffer-local LSP keymaps

--- neovim/.config/nvim/lua/user/plugins/lspconfig.lua
+++ neovim/.config/nvim/lua/user/plugins/lspconfig.lua
@@ -1,6 +1,6 @@
 -- Mappings
--- See `:help vim.diagnostic.*` for documentation on any of the functions below
 local keymap = vim.keymap.set  -- Sets `noremap` by default
+-- See `:help vim.diagnostic.*` for documentation on any of the functions below
 keymap('n', 'gl', vim.diagnostic.open_float)
 keymap('n', '[d', vim.diagnostic.goto_prev)
 keymap('n', ']d', vim.diagnostic.goto_next)
@@ -12,27 +12,32 @@ keymap('n', '<leader>lq', vim.diagnostic.setloclist)
 -- active in the buffer.
 local on_attach = function(client, bufnr)
   -- Mappings
+  local bufmap = function(mode, lhs, rhs, opts)
+    opts = opts or {}
+    opts.buffer = bufnr
+    keymap(mode, lhs, rhs, opts)
+  end
+
   -- See `:help vim.lsp.*` for documentation on any of the functions below
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  keymap('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  keymap('n', 'gd', vim.lsp.buf.definition, bufopts)
-  keymap('n', 'K', vim.lsp.buf.hover, bufopts)
-  keymap('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  keymap('n', 'gr', vim.lsp.buf.references, bufopts)
-  -- keymap('n', 'gr', require'telescope.builtin'.lsp_references, bufopts)
-  keymap('n', '<leader>fs', require'telescope.builtin'.lsp_document_symbols, bufopts)
-  keymap('n', '<leader>la', vim.lsp.buf.code_action, bufopts)
-  keymap('v', '<leader>la', vim.lsp.buf.range_code_action, bufopts)
-  keymap('n', '<leader>lD', vim.lsp.buf.type_definition, bufopts)
-  keymap('n', '<leader>lf', vim.lsp.buf.formatting, bufopts)
-  keymap('v', '<leader>lf', vim.lsp.buf.range_formatting, bufopts)
-  keymap('n', '<leader>lr', vim.lsp.buf.rename, bufopts)
-  keymap('n', '<leader>ls', vim.lsp.buf.signature_help, bufopts)
-  keymap('n', '<leader>lwa', vim.lsp.buf.add_workspace_folder, bufopts)
-  keymap('n', '<leader>lwr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  keymap('n', '<leader>lwl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
+  bufmap('n', 'gD', vim.lsp.buf.declaration)
+  bufmap('n', 'gd', vim.lsp.buf.definition)
+  bufmap('n', 'K', vim.lsp.buf.hover)
+  bufmap('n', 'gi', vim.lsp.buf.implementation)
+  bufmap('n', 'gr', vim.lsp.buf.references)
+  bufmap('n', 'gr', require'telescope.builtin'.lsp_references)
+  bufmap('n', '<leader>fs', require'telescope.builtin'.lsp_document_symbols)
+  bufmap('n', '<leader>la', vim.lsp.buf.code_action)
+  bufmap('v', '<leader>la', vim.lsp.buf.range_code_action)
+  bufmap('n', '<leader>lD', vim.lsp.buf.type_definition)
+  bufmap('n', '<leader>lf', vim.lsp.buf.formatting)
+  bufmap('v', '<leader>lf', vim.lsp.buf.range_formatting)
+  bufmap('n', '<leader>lr', vim.lsp.buf.rename)
+  bufmap('n', '<leader>ls', vim.lsp.buf.signature_help)
+  bufmap('n', '<leader>lwa', vim.lsp.buf.add_workspace_folder)
+  bufmap('n', '<leader>lwr', vim.lsp.buf.remove_workspace_folder)
+  bufmap('n', '<leader>lwl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end)
 end
 
 
