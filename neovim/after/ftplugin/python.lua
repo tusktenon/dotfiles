c207e95597de9f4bb62407c770110c8ae947f561
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2022-09-25 08:05:33 -0400

    Neovim: Add binding to run current Python file

--- neovim/after/ftplugin/python.lua
+++ neovim/after/ftplugin/python.lua
@@ -9,3 +9,6 @@ opt.expandtab = true
 opt.shiftwidth = 4
 opt.tabstop = 4
 opt.softtabstop = 4
+
+-- See https://stackoverflow.com/questions/18948491/running-python-code-in-vim
+vim.keymap.set('n', '<leader>cr', ":up<CR>:exec '!python3' shellescape(@%, 1)<CR>", {desc = 'Run file'})
