6e4f987197481751edaa3ed683cdc4acb3fad455
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2023-10-30 16:42:52 -0400

    Neovim: Enable Treesitter incremental selection

--- neovim/lua/user/plugins/treesitter.lua
+++ neovim/lua/user/plugins/treesitter.lua
@@ -49,6 +49,15 @@ return {
           'markdown',
         }
       },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<C-Space>',
+          node_incremental = '<C-Space>',
+          scope_incremental = false,
+          node_decremental = '<BS>',
+        }
+      },
       indent = { enable = true },
 
       -- Extra Modules
@@ -69,8 +78,6 @@ return {
         extended_mode = false,
         -- colors = {},
       },
-      -- TODO: Try incremental selection
-      -- TODO: Try indentation
       -- TODO: Set up text objects
     }
   end
