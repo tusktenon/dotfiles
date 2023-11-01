3f127b80c2126db35aef6fd8907559bda2d44bf3
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2023-11-01 08:37:57 -0400

    Neovim: Reactivate targets.vim

--- neovim/lua/user/plugins/editing.lua
+++ neovim/lua/user/plugins/editing.lua
@@ -22,32 +22,37 @@ return {
   'chaoren/vim-wordmotion',
 
   -- Adds various text objects
-  -- {
-  --   'wellle/targets.vim',
-  --   config = function()
-  --     -- Prefer multiline targets around cursor over distant targets within cursor line
-  --     -- (this is more consistent with default Vim behaviour)
-  --     vim.g.targets_seekRanges = 'cc cr cb cB lc ac Ac lr lb ar ab lB Ar aB Ab AB rr ll rb al rB Al bb aa bB Aa BB AA'
-  --   end
-  -- },
+  {
+    'wellle/targets.vim',
+    config = function()
+      -- Seek for next and last (previous) object with `n` and `p`
+      vim.g.targets_nl = {'n', 'p'}
+
+      -- Prefer multiline targets around cursor over distant targets within cursor line
+      -- (this is more consistent with default Vim behaviour)
+      vim.g.targets_seekRanges = 'cc cr cb cB lc ac Ac lr lb ar ab lB Ar aB Ab AB rr ll rb al rB Al bb aa bB Aa BB AA'
+    end
+  },
 
   -- Text Objects
   {
     -- A plugin that lets you create your own text objects
     'kana/vim-textobj-user',
     dependencies = {
-      {
-        -- Redefines 'ab' and 'ib' for any type of brace: (, [, {, <
-        'rhysd/vim-textobj-anyblock',
-        config = function()
-          vim.cmd([[ let g:textobj#anyblock#blocks = ['(', '[', '{', '<'] ]])
-        end
-      },
+      -- {
+      --   -- Redefines 'ab' and 'ib' for any type of brace: (, [, {, <
+      --   -- NOTE: targets.vim also provides this funcitonality
+      --   'rhysd/vim-textobj-anyblock',
+      --   config = function()
+      --     vim.cmd([[ let g:textobj#anyblock#blocks = ['(', '[', '{', '<'] ]])
+      --   end
+      -- },
       -- Adds 'ae' and 'ie' for the entire buffer
       'kana/vim-textobj-entire',
 
       -- Adds 'aq' and 'iq' for any pair of quotes of any type (single, double or backtick)
-      'beloglazov/vim-textobj-quotes',
+      -- NOTE: targets.vim also provides this funcitonality
+      -- 'beloglazov/vim-textobj-quotes',
 
       -- Adds 'ax' and 'ix' for HTML/XML attribute
       'whatyouhide/vim-textobj-xmlattr',
