49f9bb3d97b060fb422e1c9841bc6778bc9aeb92
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2023-11-01 07:34:13 -0400

    Neovim: Add two text-object plugins

--- neovim/lua/user/plugins/editing.lua
+++ neovim/lua/user/plugins/editing.lua
@@ -22,23 +22,38 @@ return {
   'chaoren/vim-wordmotion',
 
   -- Adds various text objects
-  {
-    'wellle/targets.vim',
-    config = function()
-      -- Prefer multiline targets around cursor over distant targets within cursor line
-      -- (this is more consistent with default Vim behaviour)
-      vim.g.targets_seekRanges = 'cc cr cb cB lc ac Ac lr lb ar ab lB Ar aB Ab AB rr ll rb al rB Al bb aa bB Aa BB AA'
-    end
-  },
+  -- {
+  --   'wellle/targets.vim',
+  --   config = function()
+  --     -- Prefer multiline targets around cursor over distant targets within cursor line
+  --     -- (this is more consistent with default Vim behaviour)
+  --     vim.g.targets_seekRanges = 'cc cr cb cB lc ac Ac lr lb ar ab lB Ar aB Ab AB rr ll rb al rB Al bb aa bB Aa BB AA'
+  --   end
+  -- },
 
   -- Text Objects
   {
-    'kana/vim-textobj-entire',           -- Entire buffer
-    dependencies = {'kana/vim-textobj-user'}
+    -- A plugin that lets you create your own text objects
+    'kana/vim-textobj-user',
+    dependencies = {
+      {
+        -- Redefines 'ab' and 'ib' for any type of brace: (, [, {, <
+        'rhysd/vim-textobj-anyblock',
+        config = function()
+          vim.cmd([[ let g:textobj#anyblock#blocks = ['(', '[', '{', '<'] ]])
+        end
+      },
+      -- Adds 'ae' and 'ie' for the entire buffer
+      'kana/vim-textobj-entire',
+
+      -- Adds 'aq' and 'iq' for any pair of quotes of any type (single, double or backtick)
+      'beloglazov/vim-textobj-quotes',
+
+      -- Adds 'ax' and 'ix' for HTML/XML attribute
+      'whatyouhide/vim-textobj-xmlattr',
+    }
   },
-  'michaeljsmith/vim-indent-object',     -- Indentation level
-  {
-    'whatyouhide/vim-textobj-xmlattr',   -- HTML/XML attribute
-    dependencies = {'kana/vim-textobj-user'}
-  }
+
+  -- Adds 'aI', 'ai' and 'ii' for indentation level
+  'michaeljsmith/vim-indent-object'
 }
