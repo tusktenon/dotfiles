e64ea5668ae1a9dafd3961d5dcf853d6c2cbf299
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2023-12-07 22:11:18 -0500

    Neovim: Hide hint line in Neogit

--- neovim/lua/user/plugins/neogit.lua
+++ neovim/lua/user/plugins/neogit.lua
@@ -3,7 +3,9 @@ return {
   'NeogitOrg/neogit',
   dependencies = {'nvim-lua/plenary.nvim'},
   opts = {
+    disable_hint = true,
     disable_commit_confirmation = true,
+    graph_style = "unicode",
     integrations = {
       diffview = true,  -- View diffs using `sindrets/diffview.nvim`
       telescope = true  -- Use Telescope for menu selection rather than vim.ui.select
