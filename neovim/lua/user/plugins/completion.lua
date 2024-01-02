482080770bcc46b148a771d1697a46b61d8dbce5
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-01-02 17:02:47 -0500

    Neovim: Change a completion keymap
    The suggested mapping to abort/close the completion menu is Ctrl-e, but
    using this means frequently having to enter Ctrl-e twice to jump to the
    end of the line.

--- neovim/lua/user/plugins/completion.lua
+++ neovim/lua/user/plugins/completion.lua
@@ -57,7 +57,7 @@ return {
         ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
         ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
         ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-        ["<C-e>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close(), }),
+        ["<C-c>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close(), }),
 
         -- Set `select = true` to accept currently selected item; if none selected, select first item.
         -- Set `select = false` to only confirm explicitly selected items.
