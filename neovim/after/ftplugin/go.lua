b1121cc872242c50fd1347025d91353a3d80035f
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-12-13 15:53:19 -0500

    Neovim: Use (hard) tabs in Go files

---
+++ neovim/after/ftplugin/go.lua
@@ -0,0 +1,11 @@
+-- Standard Go formatting uses the rules of `gofmt`.
+-- In particular, tabs (not spaces) are used for indentation.
+local opt = vim.opt
+
+opt.colorcolumn = '80'
+opt.textwidth = 80
+
+opt.noexpandtab = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.softtabstop = 0
