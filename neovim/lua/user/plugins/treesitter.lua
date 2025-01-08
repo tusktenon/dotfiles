8527c5fd79ff8da51023eab7b893c05bbdd3e1c8
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-01-08 10:33:00 -0500

    Neovim: fix nvim-ts-autotag setup
    Setup via `nvim-treesitter.configs` is deprecated.

--- neovim/lua/user/plugins/treesitter.lua
+++ neovim/lua/user/plugins/treesitter.lua
@@ -1,84 +1,89 @@
 -- Treesitter
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects',  -- Syntax-aware text objects with select/move/swap/peek support
-    'windwp/nvim-ts-autotag',                       -- Autopair functionality for HTLM/XML tags
-    -- 'p00f/nvim-ts-rainbow',                         -- Rainbow brackets
-  },
-  config = function()
-    require'nvim-treesitter.configs'.setup {
-      ensure_installed = {
-        'bash',
-        'c',
-        'c_sharp',
-        'cmake',
-        'comment',
-        'cpp',
-        'css',
-        'gitcommit',
-        'gitignore',
-        'go',
-        'gomod',
-        'gosum',
-        'gowork',
-        'haskell',
-        'html',
-        'java',
-        'javascript',
-        'json',
-        'latex',
-        'lua',
-        'make',
-        'markdown',
-        'markdown_inline',
-        'php',
-        'python',
-        'racket',
-        'ruby',
-        'rust',
-        'sql',
-        'toml',
-        'vim',
-        'vimdoc',
-        'xml',
-        'yaml',
-        'query',
-      },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        disable = {
-          -- TreeSitter often gets confused by multiple inline code blocks on the same line
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = function() require('nvim-treesitter.install').update { with_sync = true } end,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects', -- Syntax-aware text objects with select/move/swap/peek support
+      -- 'p00f/nvim-ts-rainbow', -- Rainbow brackets
+    },
+    config = function()
+      require('nvim-treesitter.configs').setup {
+        ensure_installed = {
+          'bash',
+          'c',
+          'c_sharp',
+          'cmake',
+          'comment',
+          'cpp',
+          'css',
+          'gitcommit',
+          'gitignore',
+          'go',
+          'gomod',
+          'gosum',
+          'gowork',
+          'haskell',
+          'html',
+          'java',
+          'javascript', -- and JSX
+          'json',
+          'latex',
+          'lua',
+          'make',
           'markdown',
-        }
-      },
-      incremental_selection = {  -- Compare the vim-expand-region plugin
-        enable = true,
-        keymaps = {
-          init_selection = '<C-Space>',
-          node_incremental = '<C-Space>',
-          scope_incremental = false,
-          node_decremental = '<BS>',
-        }
-      },
-      indent = { enable = true },
+          'markdown_inline',
+          'php',
+          'python',
+          'racket',
+          'ruby',
+          'rust',
+          'sql',
+          'toml',
+          'typescript', -- and TSX
+          'vim',
+          'vimdoc',
+          'xml',
+          'yaml',
+          'query',
+        },
+        auto_install = true,
+        highlight = {
+          enable = true,
+          disable = {
+            -- TreeSitter often gets confused by multiple inline code blocks on the same line
+            'markdown',
+          },
+        },
+        incremental_selection = { -- Compare the vim-expand-region plugin
+          enable = true,
+          keymaps = {
+            init_selection = '<C-Space>',
+            node_incremental = '<C-Space>',
+            scope_incremental = false,
+            node_decremental = '<BS>',
+          },
+        },
+        indent = { enable = true },
 
-      -- Extra Modules
-      autotag = {
-        enable = true,
-      },
-      matchup = {
-        enable = true,
-        include_match_words = true,
-      },
-      -- rainbow = {
-      --   enable = true,
-      --   disable = {'html'},
-      --   extended_mode = false,
-      --   -- colors = {},
-      -- },
-    }
-  end
+        -- Extra Modules
+        matchup = {
+          enable = true,
+          include_match_words = true,
+        },
+        -- rainbow = {
+        --   enable = true,
+        --   disable = {'html'},
+        --   extended_mode = false,
+        --   -- colors = {},
+        -- },
+      }
+    end,
+  },
+
+  -- Autopair functionality for HTML/XML tags (including JSX, TSX, etc.)
+  {
+    'windwp/nvim-ts-autotag',
+    opts = {},
+  },
 }
