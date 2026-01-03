1fb82608bdb9b732e4ef71c946f4898eba646f09
Dan Murphy <70486892+tusktenon@users.noreply.github.com>
2026-01-03 08:18:06 -0500

    Neovim: Prevent duplicate messages from Ruff

--- neovim/lua/user/plugins/linting.lua
+++ neovim/lua/user/plugins/linting.lua
@@ -9,7 +9,7 @@ return {
       -- java = { 'checkstyle' },
       javascript = { 'eslint_d' },
       typescript = { 'eslint_d' },
-      python = { 'ruff' },
+      -- Python: Ruff already provides linting
     }
 
     local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
